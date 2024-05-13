64bf33c7616dcd59bc909dae164a6eb5c2353889
mrl00 <m2l_0@outlook.com>
2024-05-13 15:37:35 -0300

    feat: create address, phonetype and phone entities

---
+++ src/main/java/com/sea/challenge/register/entities/Address.java
@@ -0,0 +1,40 @@
+package com.sea.challenge.register.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_adresses")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long addressId;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false)
+    private String publicPlace;
+
+    @Column(nullable = false)
+    private String neighborhood;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String uf;
+
+    private String complemento;
+}
---
+++ src/main/java/com/sea/challenge/register/entities/PhoneType.java
@@ -0,0 +1,7 @@
+package com.sea.challenge.register.entities;
+
+public enum PhoneType {
+    RESIDENCIAL,
+    CELPHONE,
+    COMMERCIAL,
+}
