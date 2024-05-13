8eea818517533de1aead1ef983b057fa86d78ed5
mrl00 <m2l_0@outlook.com>
2024-05-13 18:35:19 -0300

    feat: create entities
    create entity interefaces for phone, address and client classes

---
+++ src/main/java/com/sea/challenge/register/repositories/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.sea.challenge.register.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sea.challenge.register.entities.Address;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
