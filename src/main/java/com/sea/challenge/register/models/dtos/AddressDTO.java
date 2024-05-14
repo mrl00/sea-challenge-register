f7003be1a9d2ba8debe75533a0f9ba7ee92e2bd0
mrl00 <m2l_0@outlook.com>
2024-05-14 11:49:54 -0300

    feat: create client dtos

---
+++ src/main/java/com/sea/challenge/register/models/dtos/AddressDTO.java
@@ -0,0 +1,44 @@
+package com.sea.challenge.register.models.dtos;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sea.challenge.register.models.UF;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressDTO {
+
+    @Pattern(regexp = "^[0-9]{2}\\.[0-9]{3}-[0-9]{3}$", message = "{address.cep}")
+    @JsonProperty(required = true)
+    private String cep;
+
+    @NotBlank
+    @Size(max = 60, message = "{address.publicplace}")
+    @JsonProperty(required = true)
+    private String publicPlace;
+
+    @NotBlank
+    @Size(max = 100, message = "{address.neighborhood}")
+    @JsonProperty(required = true)
+    private String neighborhood;
+
+    @NotBlank
+    @Size(max = 100, message = "{address.city}")
+    @JsonProperty(required = true)
+    private String city;
+
+    @JsonProperty(required = true)
+    private UF uf;
+
+    @Size(max = 100)
+    private String complemento;
+}
---
+++ src/main/java/com/sea/challenge/register/models/dtos/PhoneRequestDTO.java
@@ -0,0 +1,17 @@
+package com.sea.challenge.register.models.dtos;
+
+import com.sea.challenge.register.models.PhoneType;
+import com.sea.challenge.register.validators.PhoneNumber;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@PhoneNumber(message = "invalid phone number")
+public class PhoneRequestDTO {
+    private PhoneType phoneType;
+    private String phone;
+}
