ac6cce3c9f866aa0d6b7e0af60d08be4523b9a18
mrl00 <m2l_0@outlook.com>
2024-05-14 11:54:56 -0300

    style: autoformat

--- src/main/java/com/sea/challenge/register/repositories/ClientRepository.java
+++ src/main/java/com/sea/challenge/register/repositories/ClientRepository.java
@@ -3,7 +3,7 @@ package com.sea.challenge.register.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sea.challenge.register.entities.Client;
+import com.sea.challenge.register.models.entities.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
