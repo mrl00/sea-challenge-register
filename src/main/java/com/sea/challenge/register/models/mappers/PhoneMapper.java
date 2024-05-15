38f8a7c8307f57af906c088ca46e78e2450add20
mrl00 <m2l_0@outlook.com>
2024-05-15 19:42:48 -0300

    refator(phone-model): normalize phone mapper

--- src/main/java/com/sea/challenge/register/models/mappers/PhoneMapper.java
+++ src/main/java/com/sea/challenge/register/models/mappers/PhoneMapper.java
@@ -18,11 +18,8 @@ public interface PhoneMapper {
         Pattern pattern = Pattern.compile("^\\((\\d{2})\\)[\\s+](\\d{4,5})[-](\\d{4})$");
         Matcher matcher = pattern.matcher(dto.getPhone());
 
-        if (matcher.matches()) {
-            phone.setDdd(matcher.group(1));
-            phone.setPrefix(matcher.group(2));
-            phone.setSuffix(matcher.group(3));
-        }
+        if (matcher.matches())
+            phone.setPhone(matcher.group(1) + matcher.group(2) + matcher.group(3));
 
         return phone;
     }
