8317a3059232e8ad1dfea77df8940460bb4ee000
mrl00 <m2l_0@outlook.com>
2024-05-15 10:49:45 -0300

    test: create client mapper test

---
+++ src/test/java/com/sea/challenge/register/mappers/ClientMapperTest.java
@@ -0,0 +1,53 @@
+package com.sea.challenge.register.mappers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sea.challenge.register.mocks.ClientMock;
+import com.sea.challenge.register.models.dtos.AddressDTO;
+import com.sea.challenge.register.models.dtos.ClientRequestDTO;
+import com.sea.challenge.register.models.dtos.PhoneDTO;
+import com.sea.challenge.register.models.entities.Address;
+import com.sea.challenge.register.models.entities.Client;
+import com.sea.challenge.register.models.entities.Phone;
+import com.sea.challenge.register.models.mappers.ClientMapper;
+
+@SpringBootTest
+public class ClientMapperTest {
+    @Autowired
+    private ClientMapper mapper;
+
+    @Test
+    public void fromRequestDTOToModelTest() {
+        ClientRequestDTO dto = ClientMock.SIMPLE_CLIENT_REQUEST_DTO;
+        Client client = mapper.fromRequestDTOToModel(dto);
+
+        AddressDTO addressDTO = dto.getAddress();
+        Address address = client.getAddress();
+
+        List<PhoneDTO> phonesDTO = dto.getPhones();
+        List<Phone> phones = client.getPhones();
+
+        // Client Fields
+        assertEquals(dto.getName(), client.getName());
+        assertEquals(dto.getEmail(), client.getEmail());
+        assertEquals(dto.getCpf(), client.getCpf());
+
+        // Client Address
+        assertEquals(addressDTO.getCep(), address.getCep());
+        assertEquals(addressDTO.getPublicPlace(), address.getPublicPlace());
+        assertEquals(addressDTO.getNeighborhood(), address.getNeighborhood());
+        assertEquals(addressDTO.getCity(), address.getCity());
+        assertEquals(addressDTO.getUf(), address.getUf());
+        assertEquals(addressDTO.getComplement(), address.getComplement());
+
+        // Client Phones
+        assertEquals(phonesDTO.get(0).getPhone(), phones.get(0).toString());
+        assertEquals(phonesDTO.get(1).getPhone(), phones.get(1).toString());
+    }
+}
