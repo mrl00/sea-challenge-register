81306c5228419860ed58055627f01d1a274d0864
mrl00 <m2l_0@outlook.com>
2024-05-15 10:49:24 -0300

    feat: create client mapper

---
+++ src/main/java/com/sea/challenge/register/models/mappers/ClientMapper.java
@@ -0,0 +1,30 @@
+package com.sea.challenge.register.models.mappers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import com.sea.challenge.register.models.dtos.ClientRequestDTO;
+import com.sea.challenge.register.models.dtos.PhoneDTO;
+import com.sea.challenge.register.models.entities.Client;
+import com.sea.challenge.register.models.entities.Phone;
+
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.FIELD)
+public interface ClientMapper {
+
+    @Mapping(target = "clientId", ignore = true)
+    @Mapping(target = "address.addressId", ignore = true)
+    @Mapping(source = "phones", target = "phones", qualifiedByName = "phonesDTOsToPhoneModels")
+    Client fromRequestDTOToModel(ClientRequestDTO dto);
+
+    @Named("phonesDTOsToPhoneModels")
+    default List<Phone> fromPhoneToPhoneDTOList(List<PhoneDTO> dtos) {
+        return dtos.stream()
+                .map(new PhoneMapperImpl()::fromDTOToModel)
+                .collect(Collectors.toList());
+    }
+}
