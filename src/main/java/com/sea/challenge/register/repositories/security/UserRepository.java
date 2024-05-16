370b7892a4bf6ca2c0c29fa969570158d3257ea2
mrl00 <m2l_0@outlook.com>
2024-05-16 19:13:35 -0300

    feat: create user repository

---
+++ src/main/java/com/sea/challenge/register/repositories/security/UserRepository.java
@@ -0,0 +1,13 @@
+package com.sea.challenge.register.repositories.security;
+
+import com.sea.challenge.register.models.entities.security.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<UserDetails> findByUserName(String userName);
+}
