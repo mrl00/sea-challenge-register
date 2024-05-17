09c7b3f9c80fbb8d95d63e244b44361b206465f9
mrl00 <m2l_0@outlook.com>
2024-05-16 21:18:48 -0300

    feat: create migration

---
+++ src/main/resources/db/migration/V1__create-tables.sql
@@ -0,0 +1,125 @@
+-- DROP SCHEMA public;
+
+-- Em H2 não é necessário criar schemas de forma explícita
+-- H2 usa a tabela INFORMATION_SCHEMA para gerenciamento de metadados
+
+-- DROP SEQUENCE public.tb_adresses_id_seq;
+
+CREATE SEQUENCE tb_adresses_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO CYCLE;
+
+-- DROP SEQUENCE public.tb_clients_id_seq;
+
+CREATE SEQUENCE tb_clients_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO CYCLE;
+
+-- DROP SEQUENCE public.tb_emails_id_seq;
+
+CREATE SEQUENCE tb_emails_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO CYCLE;
+
+-- DROP SEQUENCE public.tb_phones_id_seq;
+
+CREATE SEQUENCE tb_phones_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO CYCLE;
+
+-- DROP SEQUENCE public.tb_users_id_seq;
+
+CREATE SEQUENCE tb_users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO CYCLE;
+
+-- public.tb_adresses definition
+
+-- DROP TABLE public.tb_adresses;
+
+CREATE TABLE tb_adresses (
+    uf SMALLINT NOT NULL,
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    cep VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    complement VARCHAR(255),
+    neighborhood VARCHAR(255) NOT NULL,
+    public_place VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    CHECK (uf BETWEEN 0 AND 26)
+);
+
+-- public.tb_clients definition
+
+-- DROP TABLE public.tb_clients;
+
+CREATE TABLE tb_clients (
+    address_id BIGINT,
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    cpf VARCHAR(11) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    UNIQUE (address_id),
+    PRIMARY KEY (id)
+);
+
+-- public.tb_clients_emails definition
+
+-- DROP TABLE public.tb_clients_emails;
+
+CREATE TABLE tb_clients_emails (
+    client_id BIGINT NOT NULL,
+    emails_id BIGINT NOT NULL,
+    UNIQUE (emails_id)
+);
+
+-- public.tb_clients_phones definition
+
+-- DROP TABLE public.tb_clients_phones;
+
+CREATE TABLE tb_clients_phones (
+    client_id BIGINT NOT NULL,
+    phones_id BIGINT NOT NULL,
+    UNIQUE (phones_id)
+);
+
+-- public.tb_emails definition
+
+-- DROP TABLE public.tb_emails;
+
+CREATE TABLE tb_emails (
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    UNIQUE (email),
+    PRIMARY KEY (id)
+);
+
+-- public.tb_phones definition
+
+-- DROP TABLE public.tb_phones;
+
+CREATE TABLE tb_phones (
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    phone VARCHAR(11) NOT NULL,
+    phone_type VARCHAR(255) NOT NULL,
+    CHECK (phone_type IN ('RESIDENTIAL', 'CELLPHONE', 'COMMERCIAL')),
+    PRIMARY KEY (id)
+);
+
+-- public.tb_users definition
+
+-- DROP TABLE public.tb_users;
+
+CREATE TABLE tb_users (
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    role VARCHAR(11) NOT NULL,
+    user_name VARCHAR(64),
+    password VARCHAR(255),
+    PRIMARY KEY (id),
+    CHECK (role IN ('ADMIN', 'DEFAULT')),
+    UNIQUE (user_name)
+);
---
+++ src/main/resources/db/migration/V2__populate-user-table.sql
@@ -0,0 +1,2 @@
+insert into tb_users(ROLE, USER_NAME, PASSWORD) values ('ADMIN', 'admin', '$2a$10$ZSssLVcXwhXBw7gcCECYCOhsv6GG8GyLARS80BAWEaP0e28crhzze');
+insert into tb_users(ROLE, USER_NAME, PASSWORD) values ('DEFAULT', 'user', '$2a$10$ElLyWZROuAjgmsFOlsI5UO8IDeNsGWFS7cnyNVnZI.V5JoA1O8R/.');
