bb1d9b736c207cc02f509fb37c5f5d38f8c2a59c
mrl00 <m2l_0@outlook.com>
2024-05-18 12:44:23 -0300

    feat: update populate user migration

--- src/main/resources/db/migration/V2__populate-user-tables.sql
+++ src/main/resources/db/migration/V2__populate-user-tables.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_users (role, user_name, password) VALUES
+INSERT INTO public.tb_users (role, user_name, password) VALUES
 ('ADMIN', 'admin', '$2a$10$ZSssLVcXwhXBw7gcCECYCOhsv6GG8GyLARS80BAWEaP0e28crhzze'),
 ('DEFAULT', 'user', '$2a$10$ElLyWZROuAjgmsFOlsI5UO8IDeNsGWFS7cnyNVnZI.V5JoA1O8R/.');
